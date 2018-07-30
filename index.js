05518ac2134d5928ef6d9b507f60c74aa92d3a4a
Tony Petty <tpetty@synacor.com>
2018-07-30 17:51:58 -0400

    First commit.

---
+++ index.js
@@ -0,0 +1,30 @@
+const puppeteer = require('puppeteer');
+
+const ids = [
+"B076HY6P86",
+"B07CRQQHG2",
+"B079MFCH2Z"
+];
+
+ids.forEach((id) => {
+	(async () => {
+	  const browser = await puppeteer.launch();
+	  const page = await browser.newPage();
+	  await page.goto('https://www.audible.com/pd/' + id);
+	  const resultsSelector = '#center-1';
+	  await page.waitForSelector(resultsSelector);
+	  const title = await page.evaluate(() => document.querySelector('#center-1 > div > div > div > div.bc-col-responsive.bc-col-5 > span > ul > li.bc-list-item.bc-spacing-small > h1').innerText)
+	  const author = await page.evaluate(() => document.querySelector('#center-1 > div > div > div > div.bc-col-responsive.bc-col-5 > span > ul > li.bc-list-item.authorLabel').innerText)
+	  const narrator = await page.evaluate(() => document.querySelector('#center-1 > div > div > div > div.bc-col-responsive.bc-col-5 > span > ul > li.bc-list-item.narratorLabel').innerText)
+	  const publisher = await page.evaluate(() => document.querySelector('#center-1 > div > div > div > div.bc-col-responsive.bc-col-5 > span > ul > li.bc-list-item.publisherLabel').innerText)
+	  const description = await page.evaluate(() => document.querySelector('#center-8 > div > div > div:nth-child(2)').innerText)
+	  console.log("<hr />");
+	  console.log("<h3>IMAGE HERE</H3>");
+	  console.log("<strong>" + title + " " + author + "</strong>");
+	  console.log(narrator);
+	  console.log(publisher);
+	  console.log(description);
+
+	  await browser.close();
+	})();
+});
